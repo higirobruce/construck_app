31d7c41c8d9a15ff05728c46daab371b22a708fb
Bruce <higirobru@gmail.com>
2022-05-25 10:35:34 +0200

    disabled and loading state

--- components/common/mSubmitButton.js
+++ components/common/mSubmitButton.js
@@ -6,6 +6,7 @@ export default function MSubmitButton({
   label = 'Submit',
   icon,
   intent = '',
+  disabled = false,
 }) {
   function getClassByIntent(intent) {
     if (intent === 'primary') {
@@ -18,12 +19,17 @@ export default function MSubmitButton({
       return 'flex items-center justify-center space-x-1 bg-gray-50 rounded  ring-1 ring-zinc-300 shadow-sm cursor-pointer px-3 py-2  active:scale-95 hover:bg-gray-100 text-zinc-800'
     } else if (intent === 'success') {
       return 'flex items-center justify-center space-x-1 bg-green-100 rounded  ring-1 ring-zinc-300 shadow-sm cursor-pointer px-3 py-2  active:scale-95 hover:bg-gray-100 text-zinc-800'
+    } else if (intent === 'disabled') {
+      return 'flex items-center justify-center space-x-1 bg-gray-100 rounded  ring-1 ring-zinc-300 shadow-sm cursor-not-allowed px-3 py-2 w-20 text-zinc-800'
     } else {
       return 'flex items-center justify-center space-x-1 bg-zinc-600 rounded  ring-1 ring-zinc-300 shadow-sm cursor-pointer px-3 py-2  active:scale-95 hover:bg-zinc-500 text-white'
     }
   }
   return (
-    <button onClick={() => submit()} className={getClassByIntent(intent)}>
+    <button
+      onClick={() => (disabled ? {} : submit())}
+      className={getClassByIntent(intent)}
+    >
       {icon}
       <div className="font-bold">{label}</div>
     </button>
