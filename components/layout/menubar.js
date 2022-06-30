c27ee78e8cb744c2aeba7950fcb7ce55c73eb93d
Bruce <higirobru@gmail.com>
2022-06-30 17:06:59 +0200

    Update menubar.js
    name display fix

--- components/layout/menubar.js
+++ components/layout/menubar.js
@@ -163,19 +163,17 @@ export default function MenuBar() {
         )}
       </div>
 
-      <div className="">
-        <div className="flex flex-col">
-          <div className="mt-5 mb-1 flex flex-row items-center text-sm text-zinc-800">
-            <UserCircleIcon className="h-5 w-5" />
-            {user.firstName + ' ' + user.lastName}
-          </div>
-          <div className="flex w-full flex-row justify-evenly">
-            <LogoutIcon
-              className="mb-5 h-7 w-7 cursor-pointer text-red-400"
-              onClick={() => logout()}
-            />
-            <BellIcon className="mb-5 h-7 w-7 text-yellow-600" />
-          </div>
+      <div className="flex w-full flex-col items-center">
+        <div className="mt-5 mb-1 flex flex-row items-center text-sm text-zinc-800">
+          <UserCircleIcon className="h-5 w-5" />
+          {user.firstName + ' ' + user.lastName}
+        </div>
+        <div className="flex w-full flex-row justify-evenly">
+          <LogoutIcon
+            className="mb-5 h-7 w-7 cursor-pointer text-red-400"
+            onClick={() => logout()}
+          />
+          <BellIcon className="mb-5 h-7 w-7 text-yellow-600" />
         </div>
       </div>
     </div>
