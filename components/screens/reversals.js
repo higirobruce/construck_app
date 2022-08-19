51e40f81acbc0f9da26cf48047d0e8cb2cc4b580
construck <appinfo@construck.rw>
2022-08-19 12:50:45 +0200

    Update reversals.js

--- components/screens/reversals.js
+++ components/screens/reversals.js
@@ -6,6 +6,8 @@ import moment from 'moment'
 import MTextView from '../common/mTextView'
 import MTitle from '../common/mTitle'
 import { UserContext } from '../../contexts/UserContext'
+import { Loader } from 'semantic-ui-react'
+import Modal from '../common/modal'
 
 const { RangePicker } = DatePicker
 
@@ -15,6 +17,9 @@ export default function Reversals() {
   let [endDate, setEndDate] = useState(null)
   let [transactions, setTransactions] = useState(null)
   let url = process.env.NEXT_PUBLIC_BKEND_URL
+  let [loading, setLoading] = useState(false)
+  let [reverseTransaction, setReverseTransaction] = useState(false)
+  let [params, setParams] = useState({})
 
   let { user, setUser } = useContext(UserContext)
 
@@ -24,34 +29,52 @@ export default function Reversals() {
     )
       .then((res) => res.json())
       .then((res) => {
-        if (!res.error) setTransactions(res)
+        if (!res.error) {
+          setTransactions(res)
+          setLoading(false)
+        }
       })
-      .catch((err) => console.log(err))
+      .catch((err) => setLoading(false))
   }
 
   function refresh() {
+    setLoading(true)
     fetch(
       `${url}/works/v3/toreverse/${plateNumber}?startDate=${startDate}&endDate=${endDate}`
     )
       .then((res) => res.json())
       .then((res) => {
-        if (!res.error) setTransactions(res)
+        if (!res.error) {
+          setTransactions(res)
+          setLoading(false)
+        }
       })
-      .catch((err) => console.log(err))
+      .catch((err) => setLoading(false))
   }
 
-  function reverse(
+  function _confirmReverse(
     siteWork,
     id,
     dispatchDate,
     duration = 0,
     totalRevenue = 0,
     totalExpenditure = 0
-    // , siteWork, id, date, revenue, expenditure,
   ) {
-    if (siteWork) {
+    setParams({
+      siteWork,
+      id,
+      dispatchDate,
+      duration,
+      totalRevenue,
+      totalExpenditure,
+    })
+    setReverseTransaction(true)
+  }
+
+  function reverse() {
+    if (params.siteWork) {
       fetch(
-        `${url}/works/swreverse/${id}?date=${dispatchDate}&totalRevenue=${totalRevenue}&totalExpenditure=${totalExpenditure}&duration=${duration}`,
+        `${url}/works/swreverse/${params.id}?date=${params.dispatchDate}&totalRevenue=${params.totalRevenue}&totalExpenditure=${params.totalExpenditure}&duration=${params.duration}`,
         {
           method: 'PUT',
           headers: { 'Content-Type': 'application/json' },
@@ -65,9 +88,9 @@ export default function Reversals() {
           console.log(res)
           refresh()
         })
-        .catch((err) => console.log(err))
+        .catch((err) => setLoading(false))
     } else {
-      fetch(`${url}/works/reverse/${id}`, {
+      fetch(`${url}/works/reverse/${params.id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -78,7 +101,7 @@ export default function Reversals() {
         .then((res) => {
           refresh()
         })
-        .catch((err) => console.log(err))
+        .catch((err) => setLoading(false))
     }
   }
 
@@ -115,63 +138,78 @@ export default function Reversals() {
 
       {/* List of posted records */}
       <div className="flex flex-col space-y-1 overflow-y-auto">
-        {transactions && (
-          <>
-            <div className="grid grid-cols-8 px-2">
-              {/* Plate NUmber */}
-              <MTitle content="Plate Number" />
-              <MTitle content="Dispatch date" />
-              <MTitle content="Project" />
-              <MTitle content="Customer" />
-              <MTitle content="Site Work" />
-              <MTitle content="Duration" />
-              <MTitle content="Revenue" />
-              <MTitle content="Actions" />
-            </div>
+        {loading ? (
+          <div className="mx-auto">
+            <Loader active />
+          </div>
+        ) : (
+          transactions && (
+            <>
+              <div className="grid grid-cols-8 px-2">
+                {/* Plate NUmber */}
+                <MTitle content="Plate Number" />
+                <MTitle content="Dispatch date" />
+                <MTitle content="Project" />
+                <MTitle content="Customer" />
+                <MTitle content="Site Work" />
+                <MTitle content="Duration" />
+                <MTitle content="Revenue" />
+                <MTitle content="Actions" />
+              </div>
 
-            {transactions.map((t) => {
-              return (
-                <div className="round-sm grid grid-cols-8 items-center bg-white p-2 shadow-sm">
-                  {/* Plate NUmber */}
-                  <MTextView content={t?.equipment?.plateNumber} />
-                  <MTextView
-                    content={moment(t?.dispatchDate).format('DD-MMM-YYYY')}
-                  />
-                  <MTextView content={t?.project?.prjDescription} />
-                  <MTextView content={t?.project?.customer} />
-                  <MTextView content={t?.siteWork ? 'Yes' : 'No'} />
-                  <MTextView
-                    content={t?.duration + ' ' + t?.equipment?.uom + 's'}
-                  />
-                  <MTextView
-                    content={
-                      parseFloat(t?.totalRevenue).toLocaleString() + ' RWF'
-                    }
-                  />
+              {transactions.map((t) => {
+                return (
+                  <div className="round-sm grid grid-cols-8 items-center bg-white p-2 shadow-sm">
+                    {/* Plate NUmber */}
+                    <MTextView content={t?.equipment?.plateNumber} />
+                    <MTextView
+                      content={moment(t?.dispatchDate).format('DD-MMM-YYYY')}
+                    />
+                    <MTextView content={t?.project?.prjDescription} />
+                    <MTextView content={t?.project?.customer} />
+                    <MTextView content={t?.siteWork ? 'Yes' : 'No'} />
+                    <MTextView
+                      content={t?.duration + ' ' + t?.equipment?.uom + 's'}
+                    />
+                    <MTextView
+                      content={
+                        parseFloat(t?.totalRevenue).toLocaleString() + ' RWF'
+                      }
+                    />
 
-                  <div
-                    onClick={() =>
-                      reverse(
-                        t?.siteWork,
-                        t?._id,
-                        t?.dispatchDate,
-                        t?.equipment?.uom === 'day'
-                          ? t?.duration
-                          : t?.duration * (1000 * 60 * 60),
-                        t?.totalRevenue,
-                        t?.totalExpenditure
-                      )
-                    }
-                    className="flex h-8 w-8 cursor-pointer items-center justify-center rounded bg-red-500 p-1 text-white shadow-sm hover:bg-red-600 active:bg-red-500"
-                  >
-                    <RefreshIcon className="h-5 w-5" />
+                    <div
+                      onClick={() =>
+                        _confirmReverse(
+                          t?.siteWork,
+                          t?._id,
+                          t?.dispatchDate,
+                          t?.equipment?.uom === 'day'
+                            ? t?.duration
+                            : t?.duration * (1000 * 60 * 60),
+                          t?.totalRevenue,
+                          t?.totalExpenditure
+                        )
+                      }
+                      className="flex h-8 w-8 cursor-pointer items-center justify-center rounded bg-red-500 p-1 text-white shadow-sm hover:bg-red-600 active:bg-red-500"
+                    >
+                      <RefreshIcon className="h-5 w-5" />
+                    </div>
                   </div>
-                </div>
-              )
-            })}
-          </>
+                )
+              })}
+            </>
+          )
         )}
       </div>
+      {reverseTransaction && (
+        <Modal
+          title="Reverse transaction"
+          body="Are you sure you want to reverse this transaction? Please note that this is IRREVERSIBLE!"
+          isShown={reverseTransaction}
+          setIsShown={setReverseTransaction}
+          handleConfirm={reverse}
+        />
+      )}
     </div>
   )
 }
