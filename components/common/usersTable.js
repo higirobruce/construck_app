991df3cb5fb2b2949431c81412c09ff775d19305
higirobruce <higirobru@gmail.com>
2022-10-07 11:21:35 +0200

    Limit seen users

--- components/common/usersTable.js
+++ components/common/usersTable.js
@@ -73,10 +73,17 @@ export default function UsersTable({
     user.userType === 'customer-admin' ||
     user.userType === 'customer-project-manager'
 
-  // if (user.role === 'agent-admin')
-  //   filteredData = data.filter(
-  //     (data) => user.agency === data.agency && data.role !== 'agent-admin'
-  //   )
+  console.log(data)
+
+  if (user.userType === 'customer-project-manager')
+    filteredData = data.filter((data) => {
+      return (
+        user?.assignedProject?.prjDescription ===
+          data?.assignedProject?.prjDescription &&
+        data?.userType !== 'customer-admin' &&
+        data?.userType !== 'customer-project-manager'
+      )
+    })
 
   function handlePageChange(e, data) {
     setPageNumber(data.activePage)
@@ -85,90 +92,96 @@ export default function UsersTable({
   const pData = paginate(filteredData, pageNumber, pageSize).pagedData
   return (
     <div className="hidden w-full md:block">
-      <Table size="tiny" compact>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Full Names</Table.HeaderCell>
-            <Table.HeaderCell>Username</Table.HeaderCell>
-            <Table.HeaderCell>Email</Table.HeaderCell>
-            <Table.HeaderCell>Phone</Table.HeaderCell>
-            <Table.HeaderCell>Role</Table.HeaderCell>
-            {isCustomer && <Table.HeaderCell>Project</Table.HeaderCell>}
+      {pData?.length > 0 && (
+        <>
+          <Table size="tiny" compact>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Full Names</Table.HeaderCell>
+                <Table.HeaderCell>Username</Table.HeaderCell>
+                <Table.HeaderCell>Email</Table.HeaderCell>
+                <Table.HeaderCell>Phone</Table.HeaderCell>
+                <Table.HeaderCell>Role</Table.HeaderCell>
+                {isCustomer && <Table.HeaderCell>Project</Table.HeaderCell>}
 
-            <Table.HeaderCell>Status</Table.HeaderCell>
-            <Table.HeaderCell>Actions</Table.HeaderCell>
-            {/* <Table.HeaderCell>Created on</Table.HeaderCell>
+                <Table.HeaderCell>Status</Table.HeaderCell>
+                <Table.HeaderCell>Actions</Table.HeaderCell>
+                {/* <Table.HeaderCell>Created on</Table.HeaderCell>
             <Table.HeaderCell>Created by</Table.HeaderCell>
             <Table.HeaderCell>Permit</Table.HeaderCell>
             <Table.HeaderCell>Duration</Table.HeaderCell> */}
-          </Table.Row>
-        </Table.Header>
+              </Table.Row>
+            </Table.Header>
 
-        <Table.Body>
-          {pData.map((row) => {
-            return (
-              <Table.Row key={row._id}>
-                <Table.Cell>
-                  <MTextView content={row.firstName + ' ' + row.lastName} />
-                </Table.Cell>
-                <Table.Cell>
-                  <MTextView content={row.email} />
-                </Table.Cell>
-                <Table.Cell>
-                  <MTextView content={row.username} />
-                </Table.Cell>
-                <Table.Cell>
-                  <MTextView content={row.phone} />
-                </Table.Cell>
-                <Table.Cell>
-                  <MTextView content={row.userType} />
-                </Table.Cell>
-                {isCustomer && (
-                  <Table.Cell>
-                    <MTextView content={row.assignedProject?.prjDescription} />
-                  </Table.Cell>
-                )}
-                <Table.Cell>
-                  <MTextView content={row.status} />
-                </Table.Cell>
+            <Table.Body>
+              {pData.map((row) => {
+                return (
+                  <Table.Row key={row._id}>
+                    <Table.Cell>
+                      <MTextView content={row.firstName + ' ' + row.lastName} />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <MTextView content={row.email} />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <MTextView content={row.username} />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <MTextView content={row.phone} />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <MTextView content={row.userType} />
+                    </Table.Cell>
+                    {isCustomer && (
+                      <Table.Cell>
+                        <MTextView
+                          content={row.assignedProject?.prjDescription}
+                        />
+                      </Table.Cell>
+                    )}
+                    <Table.Cell>
+                      <MTextView content={row.status} />
+                    </Table.Cell>
 
-                <Table.Cell>
-                  <div className="mr-2 flex flex-row">
-                    <div
-                      onClick={() => handleResetPassword(row)}
-                      className="mr-4 flex h-8 w-11 cursor-pointer items-center justify-evenly rounded-full bg-white p-2 shadow-md hover:scale-105 active:scale-95 active:shadow-sm"
-                    >
-                      <ArrowPathIcon className="h-5 w-5 text-blue-400" />
-                    </div>
-                    <div
-                      onClick={() => handleChange(row)}
-                      className="mr-4 flex h-8 w-11 cursor-pointer items-center justify-evenly rounded-full bg-white p-2 shadow-md hover:scale-105 active:scale-95 active:shadow-sm"
-                    >
-                      <PencilSquareIcon className="h-5 w-5 text-green-400" />
-                    </div>
+                    <Table.Cell>
+                      <div className="mr-2 flex flex-row">
+                        <div
+                          onClick={() => handleResetPassword(row)}
+                          className="mr-4 flex h-8 w-11 cursor-pointer items-center justify-evenly rounded-full bg-white p-2 shadow-md hover:scale-105 active:scale-95 active:shadow-sm"
+                        >
+                          <ArrowPathIcon className="h-5 w-5 text-blue-400" />
+                        </div>
+                        <div
+                          onClick={() => handleChange(row)}
+                          className="mr-4 flex h-8 w-11 cursor-pointer items-center justify-evenly rounded-full bg-white p-2 shadow-md hover:scale-105 active:scale-95 active:shadow-sm"
+                        >
+                          <PencilSquareIcon className="h-5 w-5 text-green-400" />
+                        </div>
 
-                    {/* <div
+                        {/* <div
                       onClick={() => handelUpdateStatus(row, 'suspended')}
                       className="flex h-8 w-11 cursor-pointer items-center justify-evenly rounded-full bg-white p-2 shadow-md hover:scale-105 active:scale-95 active:shadow-sm"
                     >
                       <XIcon className="h-5 w-5 text-red-400" />
                     </div> */}
-                  </div>
-                </Table.Cell>
-                {/* <Table.Cell>{row.createdOn}</Table.Cell>
+                      </div>
+                    </Table.Cell>
+                    {/* <Table.Cell>{row.createdOn}</Table.Cell>
                 <Table.Cell>{row.createdBy}</Table.Cell>
                 <Table.Cell>{row.permit}</Table.Cell>
                 <Table.Cell>{row.duration}</Table.Cell> */}
-              </Table.Row>
-            )
-          })}
-        </Table.Body>
-      </Table>
-      <MPagination
-        count={data.length}
-        onPageChange={handlePageChange}
-        pageSize={pageSize}
-      />
+                  </Table.Row>
+                )
+              })}
+            </Table.Body>
+          </Table>
+          <MPagination
+            count={data.length}
+            onPageChange={handlePageChange}
+            pageSize={pageSize}
+          />
+        </>
+      )}
     </div>
   )
 }
