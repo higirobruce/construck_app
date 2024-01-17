aac939b0bd0bd45660fde9f7072cc8d12ceed01a
Bruce Higiro <higirobru@gmail.com>
2024-01-17 15:13:28 +0200

    Driver update

--- components/screens/workdata.js
+++ components/screens/workdata.js
@@ -271,12 +271,12 @@ export default function Workdata() {
         //   value: 'NA',
         //   text: 'Not applicable',
         // })
-        if (viewPort === 'edit')
-          userOptions?.push({
-            key: row?.driver?._id,
-            value: row?.driver?._id,
-            text: row?.driver?.firstName + ' ' + row?.driver?.lastName,
-          })
+        // if (viewPort === 'edit')
+          // userOptions?.push({
+          //   key: row?.driver?._id,
+          //   value: row?.driver?._id,
+          //   text: row?.driver?.firstName + ' ' + row?.driver?.lastName,
+          // })
         setDriverList(userOptions)
         seLowBedDriverList(userOptions)
         let _drLists = [userOptions]
